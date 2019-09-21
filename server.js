8ccb7ff3c5dfe9aa7f2b90a3503648011d39b045
sujithmp <sujith.mpEve1991@gmail.com>
2019-09-21 17:19:06 +0530

    unneccessary files removed

--- server.js
+++
@@ -1,4 +0,0 @@
-var http = require('http');
-var app =  require('./app')
-http.createServer(app.requestHandler).listen(1137);
-console.log("The server is running now");
--- sltldgrs.php
+++
@@ -1,9 +0,0 @@
-<?php 
-public function actionSelectLedgers(){
-	$master_model = FinanceMasterSettings::model()->findByAttributes(array('company_id'=>Setup::get_user_company_id()));
-	if($master_model->step_completed <=3){
-		/**/
-	}
-}
-
-?>
