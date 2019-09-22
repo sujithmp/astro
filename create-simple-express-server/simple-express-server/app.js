5c639af3a741c7e16747c8f6fade28f22fa30ac0
sujithmp <sujith.mpEve1991@gmail.com>
2019-09-22 19:11:52 +0530

    registeration| login|contact form submission and usage of nodemailer

--- create-simple-express-server/simple-express-server/app.js
+++ create-simple-express-server/simple-express-server/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const bodyParser = require('body-parser');
+const nodeMailer = require('nodemailer');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,8 +17,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- create-simple-express-server/simple-express-server/routes/users.js
+++ create-simple-express-server/simple-express-server/routes/users.js
@@ -1,9 +1,86 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require('nodemailer');
+console.log("nodemailer",nodemailer);
+/* POST user login */
 
-/* GET users listing. */
+router.post('/login',function(req,res,next){
+
+	/* 
+		user login is successfull ,
+		will show the home page
+	*/
+	res.render('home');
+});
+/* GET users login */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+	/*
+		send is available only with express
+		tried to use it normally 
+		but got an error  as send is not a function
+	*/	
+	res.render('login-form');
+  	//res.send('respond with a resource');
+});
+/* POST user registeration detail  */
+router.post('/register/submit', ( req, res,next) => { 
+	/* 
+		user  register successfull, will send
+		a mail notifying
+		once user successfully submitted
+		redirects to login page
+	*/
+});
+/* GET  user register */
+router.get('/register',(req,res,next) =>{
+	/* 
+		will show here the user registeration
+		form 
+	*/
+	res.render('register-form');
+});
+
+/* show  contact form */
+
+router.get('/contact', ( req, res,next) => { 
+	/* 
+		render contact form
+	*/
+
+	res.render('contact-form');
+});
+/* POST contact  */
+router.post('/contact/send', (req, res,next) => { 
+	var transport = nodemailer.createTransport({
+		service : 'Gmail',
+		auth:{
+			user : 'test.@gmail.com',
+			pass : ''
+		}
+	});
+	var mailOptions = {
+		from:'test <test.@gmail.com>',
+		to:'test.@gmail.com',
+		subject:"This is a test mail from I-learn",
+		text:"Please see the details below ",
+		html:'<p> Please see the below details<p>'+
+				'<ul>'+
+				'<li> Name:'+req.body.username+'</li>'+
+				'<li>Email:'+req.body.email+'</li>'+
+				'<li>Message:'+req.body.message+'</li>'+
+				'</ul>'	,
+	};
+	//console.log("transport",transport);
+	transport.sendMail(mailOptions, (error,info) => { 
+		if(error){
+			console.log("Message not sent"+ error.message);
+			res.redirect('/');
+		}else{
+			 console.log("Message sent");;
+			res.redirect('/');
+		}
+	});
 });
 
 module.exports = router;
