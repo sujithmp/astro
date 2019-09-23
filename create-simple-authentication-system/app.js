85fa7cd3539b153c732c3e781b6881ac5f7d7c1f
sujithmp <sujith.mpEve1991@gmail.com>
2019-09-23 21:18:40 +0530

    passport based authentication

---
+++ create-simple-authentication-system/app.js
@@ -0,0 +1,108 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressMessage = require('express-messages');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const multer = require('multer');
+var uploads = multer({dest:"./uploads"});
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+// Handle file upload
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//  Handle session
+app.use(session({
+	secret:"secret",
+	saveUninitialized:true,
+	resave:true
+
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Validator
+app.use(expressValidator({
+	errorFormatter:function(param,msg,value){
+		var namespace = param.split('.')
+		,root = namespace.shift()
+		,formParam = root;
+		while(namespace.length){
+			formParam+= '[' + namespace.shift() + ']';
+		}
+		return {
+			param : formParam,
+			msg : msg,
+			value : value
+		};
+	}
+
+}))
+
+// middle ware
+app.use(flash());
+app.use(function(req, res, next){
+    //res.locals.success_messages = req.flash('success_messages');
+    res.locals.messages = expressMessage(req,res);
+    next();
+});
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
